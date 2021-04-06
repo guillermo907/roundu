9ee3621ba5be0a166753632c0a77f90893c5b854
Guillermo Lòpez Garcìa <willy@MacBook-Pro-de-Guillermo.local>
2021-04-05 20:37:04 -0500

    first commit2

--- src/main.js
+++ src/main.js
@@ -1,27 +1,7 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import router from './router';
 import App from './App.vue'
 
-import Home from './components/Home'
-import About from './components/About'
-
-const routes = [
-    {
-        path: '/',
-        component: Home
-    },
-    {
-        path: '/about',
-        component: About
-    }
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
-
 const app = createApp(App);
-
 app.use(router);
 app.mount('#app');
